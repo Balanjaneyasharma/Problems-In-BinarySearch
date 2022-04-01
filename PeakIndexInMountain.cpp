48280910f79184ff49e8e2f51adabd280d23e361
Balu Shrama <69688211+Balanjaneyasharma@users.noreply.github.com>
2022-04-01 22:00:43 +0530

    Update PeakIndexInMountain.cpp

--- PeakIndexInMountain.cpp
+++ PeakIndexInMountain.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    //Leetcode
     int peakIndexInMountainArray(vector<int>& arr) {
         int l=0,h=arr.size()-1;
         while(l<=h){
@@ -10,4 +11,17 @@ public:
         }
         return l;  
     }
+    //GeekForGeeks
+    int peakElement(int arr[], int n)
+    {
+       // Your code here
+       int l =0,r=n-1;
+       while(l<r){
+           int mid = l+(r-l)/2;
+            if (arr[mid]>arr[mid+1]) r = mid;
+           else l= mid+1;
+       }
+       return l;
+    }
+};
 };
